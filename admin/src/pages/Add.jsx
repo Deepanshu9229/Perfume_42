3d6268294f056df7ef7515c3947c7ed061fa7e64
Deepa <deepanshupatel9229@gmail.com>
2025-07-11 16:44:00 +0530

    admin pannel - remove product done

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { assets } from '../assets/asset.js'
 import axios from "axios"
 import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
 
 const Add = ({token}) => {
 
@@ -36,12 +37,22 @@ const Add = ({token}) => {
       image3 && formData.append("image3", image3)
 
       const response = await axios.post(backendUrl + "/api/product/add", formData, {headers:{token}})
-      console.log(response);
+      // console.log(response);
+
+      if(response.data.success){
+        toast.success(response.data.message)
+        setName('')
+        setDescription("")
+        setPrice("")
+        setImage1(false)
+        setImage2(false)
+        setImage3(false)
+      } else toast.error(response.data.message)
       
 
     } catch (error) {
       console.log(error);
-
+      toast.error(error.message)
      }
   }
 
