07b61de0447587d39d8707fe3f6324c22c6fa51b
Deepa <deepanshupatel9229@gmail.com>
2025-07-16 23:52:05 +0530

    Fix: Correct file extension for ShopContext import

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import CartTotal from '../components/cartTotal'
+import CartTotal from '../components/CartTotal'
 import { assets } from '../assets/assets.js'
 import { ShopContext } from '../context/ShopContext.jsx'
 import { toast } from 'react-toastify'
